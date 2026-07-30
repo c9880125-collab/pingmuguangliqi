13afbc438f4f6244c0e59e97ab2832a1088c5ba7
c9880125-collab <c9880125@gmail.com>
2026-07-31 02:22:59 +0800

    Add files via upload

--- composeApp/rec/commonMain/kotlin/com/refreshrate/control/model/AppInfo.kt
+++ composeApp/rec/commonMain/kotlin/com/refreshrate/control/model/AppInfo.kt
@@ -1,1 +1,11 @@
-1
+package com.refreshrate.control.model
+
+data class AppInfo(
+    val name: String,
+    val packageName: String,
+    val systemApp: Boolean = false,
+    val userId: Int = 0
+) {
+    val effectivePkg: String
+        get() = if (userId > 0) "$packageName:u$userId" else packageName
+}
